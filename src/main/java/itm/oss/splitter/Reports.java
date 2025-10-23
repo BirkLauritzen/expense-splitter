d517c3598e670ae00f7c9dd93a822316bda7f527
Matias Tarvainen <tarvainen.matias@gmail.com>
2025-10-23 15:41:16 +0900

    Test folder and example csv files created

--- src/main/java/itm/oss/splitter/Reports.java
+++ src/main/java/itm/oss/splitter/Reports.java
@@ -14,19 +14,26 @@ public class Reports {
     public SimplePersonSummaryMap perPerson(ArrayList<Expense> xs) {
         SimplePersonSummaryMap map = new SimplePersonSummaryMap();
 
+        // xs is a list of Expence objects
+        // String date, String payer, BigDecimal amount, String currency,
+        // ArrayList<String> participants, String category, String notes
         for (Expense x : xs) {
             int count = x.getParticipants().size();
             if (count == 0) continue;
 
+            // amount paid divided by people in the bill
             BigDecimal owedPerPerson = x.getAmount()
                     .divide(new BigDecimal(count), 2, RoundingMode.HALF_UP);
 
             for (String name : x.getParticipants()) {
+
+                // create new payment map for each participant
                 if (map.get(name) == null) {
                     map.put(name, new PersonSummary(BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO ));
                 }
                 PersonSummary ps = map.get(name);
 
+                // add full amount to payer
                 if (name.equals(x.getPayer())) {
                     ps.addPaid(x.getAmount());
                 }
